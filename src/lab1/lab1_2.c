6fa471f757fa77664e7bca89bbe9d193d4c25dc5
Armand-Cezar Mathe <me@cezarmathe.com>
2021-10-27 18:05:40 +0300

    Add `lab1_2`

---
+++ src/lab1/lab1_2.c
@@ -0,0 +1,125 @@
+/*
+ * lab1_2.c
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lab1_2.h"
+
+stack_t stack_new(int capacity) {
+    // declare the stack
+    stack_t stack;
+
+    // allocate the array that will hold the elements of this stack
+    // capacity    -> number of elements
+    // sizeof(int) -> the size of one element (an integer)
+    stack.data = malloc(capacity * sizeof(int));
+    // assign the capacity
+    stack.cap = capacity;
+    // set the length at 0 (since there are no elements yet)
+    stack.len = 0;
+
+    // return the newly-created stack
+    return stack;
+}
+
+void stack_free(stack_t * stack) {
+    // set the capacity to 0 (the stack will not be able to hold elements
+    // anymore)
+    stack->cap = 0;
+    // set the length to 0 (the elements will be deallocated)
+    stack->len = 0;
+    // deallocate the array that holds the elements in this stack
+    free(stack->data);
+}
+
+bool stack_empty(stack_t * stack) {
+    // the stack is empty if it's length is equal to 0 (aka if there are no
+    // elements)
+    return stack->len == 0;
+}
+
+int stack_pop(stack_t * stack) {
+    // if the stack is empty, there are no elements we can pop
+    if (stack_empty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    // get the value from the top of the stack
+    int value = stack->data[stack->len - 1];
+    // decrease the length by 1 to indicate that this element was removed
+    stack->len -= 1;
+
+    // return the popped value
+    return value;
+}
+
+void stack_push(stack_t * stack, int value) {
+    // if the stack is full, we can't push any more elements onto the stack
+    //
+    // the original implementation does not check if the stack is full. try
+    // commenting the next four lines and see what happens when you push more
+    // items than the stack can hold (depends on the capacity you give it)
+    if (stack->len == stack->cap) {
+        printf("The stack is full\n");
+        return;
+    }
+
+    // add the value at the top of the stack
+    stack->data[stack->len] = value;
+    // increase the length (number of elements) by 1
+    stack->len += 1;
+}
+
+void stack_print(stack_t * stack) {
+    // iterate over each element and print it
+    for (int i = 0; i < stack->len; i++) {
+        printf("%d ", stack->data[i]);
+    }
+    // print a new line (for aesthetic purposes)
+    printf("\n");
+}
+
+int main() {
+    // initialize a new stack
+    stack_t stack = stack_new(20);
+
+    // initialize the element
+    int element = -1;
+
+    // while a non-null element is passed
+    while (element != 0) {
+        printf("Append a number (or 0 to stop): ");
+        // read an element. no checks are made to see if this is an actual
+        // number. see what happens when you try to use a letter instead
+        scanf("%d", &element);
+
+        // push the element onto the stack
+        stack_push(&stack, element);
+
+        printf("Contents of the stack: ");
+        // and then print the contents of the stack
+        stack_print(&stack);
+    }
+
+    printf("Done\n");
+
+    // while the stack is not empty
+    while (!stack_empty(&stack)) {
+        // pop a value off the stack
+        int popped_value = stack_pop(&stack);
+        printf("Popped a value off the stack: %d\n", popped_value);
+
+        printf("Contents of the stack: ");
+        // and then print the contents of the stack
+        stack_print(&stack);
+    }
+
+    // deallocate the stack
+    stack_free(&stack);
+
+    // return ok
+    return 0;
+}
---
+++ src/lab1/lab1_2.h
@@ -0,0 +1,87 @@
+/*
+ * lab1_2.h
+ */
+
+#include <stdbool.h>
+
+#ifndef FSEGA_IE2_DSA_LAB1_2_H
+#define FSEGA_IE2_DSA_LAB1_2_H
+
+/**
+ * @brief A heap-allocated, non-growable stack.
+ */
+typedef struct stack_t {
+    /**
+     * @brief Heap-allocated array that stores the data in this stack.
+     */
+    int * data;
+
+    /**
+     * @brief Maximum capacity that can be stored in this stack.
+     *
+     * @details The capacity is the maximum number of elements that can be
+     * stored in this stack - this means `len` must be less than or equal to
+     * `cap`.
+     *
+     * @attention Not to be confused with length!
+     */
+    int cap;
+
+    /**
+     * @brief Current number of elements in the stack.
+     *
+     * @attention Not to be confused with capacity!
+     */
+    int len;
+} stack_t;
+
+/**
+ * @brief Create a new stack.
+ *
+ * @param capacity the maximum capacity the stack can hold
+ * @return the new stack
+ */
+stack_t stack_new(int capacity);
+
+/**
+ * @brief Deallocate a stack.
+ *
+ * @warning Calling this function for a stack renders the stack unusable!
+ *
+ * @param stack the stack
+ */
+void stack_free(stack_t * stack);
+
+/**
+ * @brief Empty a stack
+ *
+ * @param stack the stack
+ * @return true if the stack is empty, false otherwise
+ */
+bool stack_empty(stack_t * stack);
+
+/**
+ * @brief Pop a value from the stack.
+ *
+ * @param stack the stack
+ * @return the value popped from the stack, or -1 (and prints a failure message)
+ * if the stack if empty
+ */
+int stack_pop(stack_t * stack);
+
+/**
+ * @brief Push a value onto the stack.
+ *
+ * @param stack the stack
+ * @param value the value
+ */
+void stack_push(stack_t * stack, int value);
+
+/**
+ * @brief Print the contents in a human-readable format.
+ *
+ * @param stack the stack.
+ */
+void stack_print(stack_t * stack);
+
+#endif // FSEGA_IE2_DSA_LAB1_2_H
